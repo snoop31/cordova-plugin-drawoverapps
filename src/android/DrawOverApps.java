073758051e88b6e604d5a261aadbc3ace0fb77e5
Ivan <snoop31@yandex.ru>
2022-11-30 14:50:45 +0300

    Update DrawOverApps.java

--- src/android/DrawOverApps.java
+++ src/android/DrawOverApps.java
@@ -31,7 +31,6 @@ import org.json.JSONObject;
 
 import android.os.Build;
 import android.Manifest;
-//import android.support.v4.app.ActivityCompat;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -42,12 +41,27 @@ import android.net.Uri;
 import org.apache.cordova.overApps.Services.OverAppsService;
 import org.apache.cordova.overApps.Services.ServiceParameters;
 
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.content.res.Resources;
+
+import android.os.PowerManager;
+import android.content.Context;
+
 
 public class DrawOverApps extends CordovaPlugin {
 		private static final String TAG = "DrawOverApps";
 	  private static final String ACTION_OPEN_OVER_APP_VIEW = "open";
 		private static final String ACTION_CLOSE_OVER_APP_VIEW = "close";
-		private static final String ACTION_CHECK_OVER_APP_PERMISSION = "checkPermission";
+
+		private static final String ACTION_CHECK_OVER_APP_PERMISSION = "checkOverAppPermission";
+		private static final String ACTION_REQUEST_OVER_APP_PERMISSION = "requestOverAppPermission";
+		private static final String ACTION_OPEN_OVER_APP_PERMISSION = "openOverAppPermission";
+
+		private static final String ACTION_CHECK_BATTERY_OPTIMIZATION = "checkBatteryOptimization";
+		private static final String ACTION_REQUEST_BATTERY_OPTIMIZATION = "requestBatteryOptimization";
+
+		private static final String ACTION_OPEN_NOTIFICATION_SETTINGS = "openNotificationSettings";
 	  private static final String FOLDER_PATH = "file:///android_asset/www/";
 		public Activity activity;
 		public ServiceParameters serviceParameters;
@@ -63,24 +77,89 @@ public class DrawOverApps extends CordovaPlugin {
 					serviceParameters = new ServiceParameters(activity);
 
 					if (action.equals(ACTION_CHECK_OVER_APP_PERMISSION)) {
-
-								if (Build.VERSION.SDK_INT >= 23) {
-										if (!checkDrawOverAppsPermission(activity)) {
-												Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,Uri.parse("package:" + activity.getPackageName()));
-												activity.startActivityForResult(intent, 0);
-												callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "Granting Draw Over Apps Permission "));
-										}
-										else
-										{
-												callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "Permission already Granted "));
-										}
-
+						if (Build.VERSION.SDK_INT >= 23) {
+								if (!checkDrawOverAppsPermission(activity)) {
+										callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "ERROR"));
 								}
-		            return true;
-	        		}
+								else
+								{
+										callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "OK"));
+								}
 
-					else if (action.equals(ACTION_OPEN_OVER_APP_VIEW)) {
+						}
+									return true;
+					} else
+					if (action.equals(ACTION_REQUEST_OVER_APP_PERMISSION)) {
+						if (Build.VERSION.SDK_INT >= 23) {
+								if (!checkDrawOverAppsPermission(activity)) {
+										Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,Uri.parse("package:" + activity.getPackageName()));
+										activity.startActivityForResult(intent, 0);
+										callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "Granting Draw Over Apps Permission "));
+								}
+								else
+								{
+										callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "Permission already Granted "));
+								}
 
+						}
+            			return true;
+      		} else
+					if (action.equals(ACTION_OPEN_OVER_APP_PERMISSION)) {
+						if (Build.VERSION.SDK_INT >= 23) {
+								Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,Uri.parse("package:" + activity.getPackageName()));
+								activity.startActivityForResult(intent, 0);
+								callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "OK"));
+						}
+									return true;
+					} else
+
+					if (action.equals(ACTION_CHECK_BATTERY_OPTIMIZATION)) {
+						PowerManager pm = (PowerManager) activity.getSystemService(Context.POWER_SERVICE);
+						if (Build.VERSION.SDK_INT >= 23) {
+								if (!pm.isIgnoringBatteryOptimizations(activity.getPackageName())) {
+										callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "ERROR"));
+								}
+								else
+								{
+										callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "OK"));
+								}
+
+						}
+									return true;
+					} else
+					if (action.equals(ACTION_REQUEST_BATTERY_OPTIMIZATION)) {
+						if (Build.VERSION.SDK_INT >= 23) {
+							PowerManager pm = (PowerManager) activity.getSystemService(Context.POWER_SERVICE);
+							if (!pm.isIgnoringBatteryOptimizations(activity.getPackageName())) {
+								Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS, Uri.parse("package:" + activity.getPackageName()));
+								activity.startActivityForResult(intent, 0);
+								callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "Granting Ignore Battery Optimization Permission "));
+							} else {
+								callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "Ignore Battery Optimization Permission already Granted "));
+							}
+						}
+						return true;
+					} else
+
+					if (action.equals(ACTION_OPEN_NOTIFICATION_SETTINGS)){
+						Intent intent = new Intent();
+				    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+				        intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+				        intent.putExtra(Settings.EXTRA_APP_PACKAGE, activity.getPackageName());
+				    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+				        intent.setAction("android.settings.APP_NOTIFICATION_SETTINGS");
+				        intent.putExtra("app_package", activity.getPackageName());
+				        intent.putExtra("app_uid", activity.getApplicationInfo().uid);
+				    } else {
+				        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+				        intent.addCategory(Intent.CATEGORY_DEFAULT);
+				        intent.setData(Uri.parse("package:" + activity.getPackageName()));
+				    }
+				    activity.startActivity(intent);
+						callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "OK"));
+						return true;
+					} else
+					if (action.equals(ACTION_OPEN_OVER_APP_VIEW)) {
 								if (checkDrawOverAppsPermission(activity)) {
 									if (options.getJSONObject(0).has("path")) {
 										openServiceWithHTMLfile(callbackContext,options.getJSONObject(0) );
@@ -94,9 +173,8 @@ public class DrawOverApps extends CordovaPlugin {
 								}
 
 								return true;
-					}
-					else if (action.equals(ACTION_CLOSE_OVER_APP_VIEW)){
-
+					} else
+					if (action.equals(ACTION_CLOSE_OVER_APP_VIEW)){
 						 activity.stopService(new Intent(activity, OverAppsService.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
 					}
 
@@ -108,6 +186,9 @@ public class DrawOverApps extends CordovaPlugin {
 				serviceParameters.setString(FOLDER_PATH + option.getString("path"),"file_path");
 				if (option.has("hasHead")) {
 						serviceParameters.setBoolean(option.getBoolean("hasHead"),"has_head");
+				}
+				if (option.has("layoutIcon")) {
+						serviceParameters.setString(option.getString("layoutIcon"),"layout_icon");
 				}
 				if (option.has("dragToSide")) {
 						serviceParameters.setBoolean(option.getBoolean("dragToSide"),"drag_to_side");
@@ -124,7 +205,11 @@ public class DrawOverApps extends CordovaPlugin {
 				if(option.has("horizontalPosition")) {
 					  serviceParameters.setString(option.getString("horizontalPosition"),"horizontal_position");
 				 }
-        activity.startService(new Intent(activity,OverAppsService.class));
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			     activity.startForegroundService(new Intent(activity, OverAppsService.class));
+				} else {
+			     activity.startService(new Intent(activity,OverAppsService.class));
+				}
 
     }
 
@@ -144,5 +229,12 @@ public class DrawOverApps extends CordovaPlugin {
         }
 
     }
+
+		@Override
+		public void onDestroy() {
+			Log.i("TAG","DESTROY MAIN");
+			activity.stopService(new Intent(activity, OverAppsService.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+			super.onDestroy();
+		}
 
 }
